73955e9af6ea826e254679902c229c9dcc84956d
abigeyl19 <imkabigail17@gmail.com>
2026-08-11 12:29:19 +0800

    feat: add full activity code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-m
+{
+    "git-autoconfig.configList": [
+        {
+            "user.email": "imkabigail17@gmail.com,
+            "user.name": "abigeyl19"
+        },
+        {
+            "user.email": "kim",
+            "user.name": "abigeyl",
+        }    
+    ]
+}
